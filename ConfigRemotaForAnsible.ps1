e4625503b5e91607a7243c65578b641ec41ac7cf
Francisco Junior <francisco.assissj@gmail.com>
2022-02-02 13:40:13 -0300

    Teste Projeto AnsibleWindowsUpdate

---
+++ ConfigRemotaForAnsible.ps1
@@ -0,0 +1,13 @@
+﻿Set-ExecutionPolicy Unrestricted -Force
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file
+
+$hostname = hostname
+
+winrm enumerate winrm/config/Listener > C:\temp\$hostname.txt
